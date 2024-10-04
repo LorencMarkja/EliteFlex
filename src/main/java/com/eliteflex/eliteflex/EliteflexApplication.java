15bb56710f8de054cf54a40dd493bbed56c7ba18
Lorenc <lorencmarkja@outlook.com>
2024-10-04 22:05:29 +0200

    init project

---
+++ src/main/java/com/eliteflex/eliteflex/EliteflexApplication.java
@@ -0,0 +1,13 @@
+package com.eliteflex.eliteflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliteflexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EliteflexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eliteflex/eliteflex/EliteflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliteflex.eliteflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliteflexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
